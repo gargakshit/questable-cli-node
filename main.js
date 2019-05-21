b09a48a0282cac44890f87e90ee6ab4e8455dcd7
Akshit Garg <garg.akshit@gmail.com>
2019-05-21 13:37:53 +0530

    List quests

--- main.js
+++ main.js
@@ -2,42 +2,71 @@ const React = require('react')
 const { render, Color, Box, Static } = require('ink')
 const figlet = require('figlet')
 const axios = require('axios')
+const Link = require('ink-link')
 
 const { token } = require('./config')
 
 const endpoint = 'https://api.questable.webionite.com'
 
-let ascii
-const start = async () => {
+const start = () => {
+  console.clear()
   figlet('Questable', (err, data) => {
     if (err) throw err
 
-    ascii = data
-
-    render(<Counter />)
+    render(<MainComponent ascii={data} />)
   })
 }
 
-class Counter extends React.Component {
+class MainComponent extends React.Component {
   constructor() {
     super()
 
     this.state = {
-      authenticated: 'Awaiting Authentication...',
-      got_quests: false
+      status: 'Awaiting Authentication',
+      got_quests: false,
+      dots: '',
+      quests: []
     }
   }
 
   componentDidMount() {
-    axios.get(`${endpoint}/auth?token=${token}`).then(res => {
-      if (res.data.success) {
-        this.setState(prevState => ({
-          authenticated: 'Authenticated'
-        }))
-      } else {
-        throw 'Invalid Token'
-      }
-    })
+    this.timer = setInterval(() => {
+      this.setState(prevState => ({
+        dots: prevState.dots.length == 5 ? '' : prevState.dots + '.'
+      }))
+    }, 100)
+
+    axios
+      .get(`${endpoint}/auth?token=${token}`)
+      .then(res => {
+        if (res.data.success) {
+          this.setState(prevState => ({
+            status: 'Getting Quests',
+            dots: ''
+          }))
+
+          axios.get(`${endpoint}/get_quests?token=${token}`).then(res => {
+            this.setState(prevState => ({
+              status: '',
+              dots: '',
+              quests: res.data
+            }))
+
+            res.data.map(quest => {
+              console.log(
+                `${quest.name} - ${!quest.state ? 'Incomplete' : 'Complete'}`
+              )
+            })
+
+            this.timer.unref()
+          })
+        } else {
+          handleError('Invalid Token')
+        }
+      })
+      .catch(err => {
+        handleError('Fatal Error, exitting...')
+      })
   }
 
   render() {
@@ -45,21 +74,30 @@ class Counter extends React.Component {
       <Box flexDirection='column'>
         <Box flexGrow={1}>
           <Color red>
+            <Link url='http://github.com/gargakshit/questable-cli-node'>
+              Fork this project <Color cyan>Github</Color>
+            </Link>
+            {'\n\n'}
             Welcome to {'\n'}
-            {ascii}
+            {this.props.ascii}
           </Color>
         </Box>
         <Box flexGrow={1}>
           <Color green bold>
             {'\n\n'}
-            {this.state.authenticated === 'Authenticated'
-              ? 'Getting Quests'
-              : 'Awaiting Authentication...'}
+            {`${this.state.status}${this.state.dots}`}
           </Color>
         </Box>
       </Box>
     )
   }
+}
+
+const handleError = err => {
+  console.clear()
+  console.log(err)
+
+  process.exit(1)
 }
 
 start()
